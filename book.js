09079c698a96e36807195ecb776ab42508647577
Monica <moha0001@stud.kea.dk>
2023-08-24 17:33:58 +0200

    superhelt js + html

---
+++ book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+  title: "Ringenes Herre",
+  author: "Tokien",
+  genre: ["Fantasy", "Drama", "Eventyr"],
+  pages: 751,
+  hasRead: true,
+  hasIllustrations: false,
+};
+
+console.log(firstBook.author);
