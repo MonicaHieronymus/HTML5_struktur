cafb7a6a97fbddca531a391a8a7271ca75200729
Monica <moha0001@stud.kea.dk>
2023-08-24 10:30:31 +0200

    DOM-elementer

---
+++ dom.js
@@ -0,0 +1,14 @@
+const overskrift = document.querySelector("#overskrift");
+console.log(overskrift);
+
+const undertitel = document.querySelector("#undertitel");
+console.log(undertitel);
+
+const article = document.querySelector("article:last-child");
+console.log(article);
+
+const article1 = document.querySelector("article:first-child");
+console.log(article1);
+
+const p = document.querySelector("p:last-child");
+console.log(p);
